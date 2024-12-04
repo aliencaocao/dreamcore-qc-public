f03aae079b247c31f16491cb18d6a264fbd895b0
Billy Cao <aliencaocao@gmail.com>
2024-12-04 15:15:33 +0800

    Add extend os drive and windows update powershell scripts

---
+++ windows_update.ps1
@@ -0,0 +1,6 @@
+﻿"Initializing Windows Update"
+Install-PackageProvider -Name NuGet -ForceBootstrap -force | Out-Null
+Install-Module -Name PSWindowsUpdate -AllowClobber -SkipPublisherCheck -force | Out-Null
+"Downloading and installing all available updates..."
+Install-WindowsUpdate -AcceptAll -IgnoreReboot | Out-Null
+"Windows Update finished."
